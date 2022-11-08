155e7a7e7a04d50dc300abf83a61a3d47ef27392
Raul Jr Asdillo <home.rauljrasdillo@gmail.com>
2022-11-08 21:09:51 +0800

    Add Profile model validations

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -1,3 +1,49 @@
 class Profile < ApplicationRecord
   belongs_to :user
+
+  before_validation do
+    self.contact_number = format_contact_number if contact_number
+    self.address = format_address if address
+  end
+
+  validates :birthdate,
+                  presence: true,
+                  comparison: { less_than_or_equal_to: Date.current - 18.years,
+                    message: "You must be at least 18 years old to continue." }
+  validates :address,
+                  presence: true
+  validates :nationality,
+                  presence: true
+  validates :civil_status,
+                  presence: true,
+                  inclusion: { in: ["single", "married", "divorced", "separated", "widowed"] }
+  validates :contact_number,
+                  presence: true,
+                  length: { in: 11..13,
+                    message: "Invalid format." }
+  validates :height,
+                  presence: true,
+                  comparison: { greater_than: 0 }
+  validates :weight,
+                  presence: true,
+                  comparison: { greater_than: 0 }
+  validates :sex,
+                  presence: true,
+                  inclusion: { in: ["male", "female"] }
+  validates :blood_type,
+                  presence: true,
+                  inclusion: { in: ["Unknown", "A+", "A-", "B+", "B-", "O+", "O-", "AB+", "AB-"] }
+  
+  private
+
+  def format_address
+    self.address
+      .gsub(",", ", ")
+      .split(" ").each { |word| word.capitalize! }
+      .join(" ")
+  end
+
+  def format_contact_number
+    self.contact_number.gsub(/\s/, "")
+  end
 end
