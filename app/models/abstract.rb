d1a4b6751b4125c8775848ab3474e0e9d68ac726
Raul Jr Asdillo <home.rauljrasdillo@gmail.com>
2022-11-09 17:57:45 +0800

    Add Prescription, Result, Abstract model validations

--- app/models/abstract.rb
+++ app/models/abstract.rb
@@ -1,3 +1,10 @@
 class Abstract < ApplicationRecord
   belongs_to :admission
+
+  validates :file_id,
+                  presence: true
+  validates :image_link,
+                  presence: true
+  validates :download_link,
+                  presence: true
 end
--- app/models/result.rb
+++ app/models/result.rb
@@ -1,3 +1,10 @@
 class Result < ApplicationRecord
   belongs_to :result_issue, polymorphic: true
+
+  validates :file_id,
+                  presence: true
+  validates :image_link,
+                  presence: true
+  validates :download_link,
+                  presence: true
 end
