7db48e8960bddd4a2305b49b814f941d0c37d048
Raul Jr Asdillo <home.rauljrasdillo@gmail.com>
2022-11-09 11:00:29 +0800

    Add Doctor model validations

--- app/models/doctor.rb
+++ app/models/doctor.rb
@@ -2,4 +2,22 @@ class Doctor < ApplicationRecord
   belongs_to :user
   has_and_belongs_to_many :consultations
   has_and_belongs_to_many :admissions
+
+  before_validation do
+    self.first_name = format_name(first_name) if first_name
+    self.last_name = format_name(last_name) if last_name
+  end
+
+  validates :first_name,
+                  presence: true
+  validates :last_name,
+                  presence: true
+  validates :specialty,
+                  presence: true
+  
+  private
+
+  def format_name(name)
+    name.split(" ").each { |word| word.capitalize! }.join(" ")
+  end
 end
