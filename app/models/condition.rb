a472b122e8adb7f0ff274c60de7482b8881795bf
Raul Jr Asdillo <home.rauljrasdillo@gmail.com>
2022-11-09 10:56:22 +0800

    Add Condition model validations

--- app/models/condition.rb
+++ app/models/condition.rb
@@ -1,3 +1,10 @@
 class Condition < ApplicationRecord
   belongs_to :user
+
+  validates :diganosis,
+                  presence: true
+  validates :start,
+                  presence: true,
+                  comparison: { less_than_or_equal_to: Date.current }
+
 end
