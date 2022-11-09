d1e77afc2d000bd163cc4d0f6c98a331139afc3b
Raul Jr Asdillo <home.rauljrasdillo@gmail.com>
2022-11-09 10:47:47 +0800

    Add EmergencyContact model validations

--- app/models/emergency_contact.rb
+++ app/models/emergency_contact.rb
@@ -1,3 +1,22 @@
 class EmergencyContact < ApplicationRecord
   belongs_to :user
+
+  before_validation do
+    self.contact_number = format_contact_number if contact_number
+  end
+
+  validates :full_name,
+                  presence: true
+  validates :relationship,
+                  presence: true
+  validates :contact_number,
+                  presence: true,
+                  length: { in: 11..13,
+                    message: "Invalid format." }
+
+  private
+
+  def format_contact_number
+    self.contact_number.gsub(/\s/, "")
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -8,8 +8,14 @@ class User < ApplicationRecord
   has_many :consultations
   has_many :admssions
 
+  before_validation do
+    self.first_name = format_name(first_name) if first_name
+    self.last_name = format_name(last_name) if last_name
+  end
+
   before_create do
     self.uid = SecureRandom.alphanumeric(10).upcase
+    self.email = email.downcase
     self.email_confirmed = false
   end
 
@@ -33,5 +39,9 @@ class User < ApplicationRecord
 
   def target_password?
     self.password_digest_changed?
+  end
+
+  def format_name(name)
+    name.split(" ").each { |word| word.capitalize! }.join(" ")
   end
 end
