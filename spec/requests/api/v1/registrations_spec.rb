4d9489ed6f49dfda749137bd8ca6d39c53e0363d
Raul Jr Asdillo <home.rauljrasdillo@gmail.com>
2022-11-15 19:49:18 +0800

    Initalize Rspec controller tests

--- spec/requests/api/v1/registrations_spec.rb
+++ spec/requests/api/v1/registrations_spec.rb
@@ -1,7 +1,20 @@
 require 'rails_helper'
 
 RSpec.describe "Api::V1::Registrations", type: :request do
-  describe "GET /index" do
-    pending "add some examples (or delete) #{__FILE__}"
+  let(:valid_params) { FactoryBot.attributes_for(:user) }
+  let(:invalid_params) { FactoryBot.attributes_for(:user, password: nil) }
+
+  describe "POST /register" do
+    it "creates a user with valid parameters" do
+      post "/api/v1/register", params: { register: valid_params }
+      expect(JSON.parse(response.body)["message"]).to eq("A confirmation email has been sent to verify your account.")
+      expect(response).to have_http_status(:created)
+    end
+
+    it "rejects user creation with invalid parameters" do
+      post "/api/v1/register", params: { register: invalid_params }
+      expect(JSON.parse(response.body)["errors"].size).to be > 0
+      expect(response).to have_http_status(:unprocessable_entity)
+    end
   end
 end
--- spec/requests/api/v1/sessions_spec.rb
+++ spec/requests/api/v1/sessions_spec.rb
@@ -1,7 +1,35 @@
 require 'rails_helper'
 
 RSpec.describe "Api::V1::Sessions", type: :request do
-  describe "GET /index" do
-    pending "add some examples (or delete) #{__FILE__}"
+  let!(:user) { FactoryBot.create(:user) }
+  let(:valid_params) { FactoryBot.attributes_for(:valid_login) }
+  let(:invalid_params) { FactoryBot.attributes_for(:invalid_login) }
+  
+    def simulate_login
+      post "/api/v1/login", params: { session: valid_params }
+    end
+
+  describe "POST /login" do
+
+    it "logs in the user with valid parameters" do
+      post "/api/v1/login", params: { session: valid_params }
+      expect(JSON.parse(response.body)["message"]).to eq("Logged in successfully.")
+      expect(response).to have_http_status(:ok)
+    end
+
+    it "rejects user login with invalid parameters" do
+      post "/api/v1/login", params: { session: invalid_params }
+      expect(JSON.parse(body)["error"]).to eq("Invalid login credentials.")
+      expect(response).to have_http_status(:unauthorized)
+    end
+  end
+
+  describe "POST /logout" do
+    it "logs out the authenticated user" do
+      post "/api/v1/login", params: { session: valid_params }
+      post "/api/v1/logout", headers: { "X-CSRF-Token" => cookies["CSRF-TOKEN"] }
+      expect(JSON.parse(body)["message"]).to eq("Logged out successfully.")
+      expect(response).to have_http_status(:ok)
+    end
   end
 end
