4fc935de50ec69af1f055af4bc3034a1f0648e9c
Raul Jr Asdillo <home.rauljrasdillo@gmail.com>
2023-03-18 21:41:43 +0800

    Setup cloudinary api for production

--- app/api/cloudinary/client.rb
+++ app/api/cloudinary/client.rb
@@ -4,20 +4,20 @@ module Cloudinary
   class Client
 
     def create_folder(folder_path)
-      Cloudinary::Api.create_folder("development/#{folder_path}")
+      Cloudinary::Api.create_folder("production/#{folder_path}")
     end
 
     def delete_folder(folder_path)
-      Cloudinary::Api.delete_resources_by_prefix("development/#{folder_path}")
-      Cloudinary::Api.delete_folder("development/#{folder_path}")
+      Cloudinary::Api.delete_resources_by_prefix("production/#{folder_path}")
+      Cloudinary::Api.delete_folder("production/#{folder_path}")
     end
 
     def upload_file(folder_path, file, uid)
-      Cloudinary::Uploader.upload(file, {public_id: uid, folder: "development/#{folder_path}"})["secure_url"]
+      Cloudinary::Uploader.upload(file, {public_id: uid, folder: "production/#{folder_path}"})["secure_url"]
     end
 
     def delete_file(folder_path)
-      Cloudinary::Uploader.destroy("development/#{folder_path}")
+      Cloudinary::Uploader.destroy("production/#{folder_path}")
     end
   end
   
