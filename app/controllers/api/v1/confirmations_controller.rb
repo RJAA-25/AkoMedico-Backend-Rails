97b12d535c0ad452dd789dc2561687e3fbbbcf70
Raul Jr Asdillo <home.rauljrasdillo@gmail.com>
2023-03-23 23:28:50 +0800

    Remove redirect from confirmations controller

--- app/controllers/api/v1/confirmations_controller.rb
+++ app/controllers/api/v1/confirmations_controller.rb
@@ -19,12 +19,12 @@ class Api::V1::ConfirmationsController < ApplicationController
       if @user.email_confirmed
         render json: { message: "Account has already been verified." },
         status: :accepted
-        redirect_to "#{ENV['CLIENT_ADDRESS']}/get-started", allow_other_host: true
+        # redirect_to "#{ENV['CLIENT_ADDRESS']}/get-started", allow_other_host: true
       else
         @user.update(email_confirmed: true)
         render json: { message: "Email confirmed. Account has been verified." },
                       status: :ok
-        redirect_to "#{ENV['CLIENT_ADDRESS']}/get-started", allow_other_host: true
+        # redirect_to "#{ENV['CLIENT_ADDRESS']}/get-started", allow_other_host: true
       end
     end
   end
